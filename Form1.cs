c657a417e41cb23d7e3365c6852aa286b430026b
IzabelaNajder <126763078+Izabela2001@users.noreply.github.com>
2024-05-05 17:23:41 +0200

    a

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Gym_Management_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string z = "Heloo";
+        }
     }
 }
